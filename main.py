f929ab2c404ae1e9a9a0d01b10278699b52325bb
TalhaSMinhas <talha.salman.minhas@icloud.com>
2025-05-14 14:39:13 +0200

    Removed boiler-plate endpoints, and getting true hijri date (islamic date) for South African region.

--- main.py
+++ main.py
@@ -2,11 +2,4 @@ from fastapi import FastAPI
 
 app = FastAPI()
 
-@app.get("/")
-async def root():
-    return {"message": "Hello World"}
-
-
-@app.get("/hello/{name}")
-async def say_hello(name: str):
-    return {"message": f"Hello {name}"}
+# future endpoint/routes will go here In Sha' Allah
