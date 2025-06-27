307922aacee28774e6e7e1535fa039a9f6c7d736
TalhaSMinhas <talha.salman.minhas@icloud.com>
2025-06-27 22:08:03 +0200

    Adjusted project structure and added testing.

--- src/main.py
+++ src/main.py
@@ -1,4 +1,5 @@
 from fastapi import FastAPI
+import hijri_date
 
 app = FastAPI()
 
---
+++ tests/test_hijri_date.py
@@ -0,0 +1,4 @@
+from src.hijri_date import get_hijri_date
+
+def test_hijri_date_returns_correct_hijri_date():
+    assert get_hijri_date()
